0d6e2aef718913f332c7ec84ea2a14ef43a57613
Esther Nieto Medina <163842038+esthernieto@users.noreply.github.com>
2024-10-10 08:54:57 +0200

    Add files via upload

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,21 +1,19 @@
 extends CharacterBody2D
 
-
 const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
 
-
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Agregar la gravedad.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += ProjectSettings.get_setting("physics/2d/default_gravity") * delta
 
-	# Handle jump.
+	# Manejar el salto.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Obtener la dirección de input y manejar el movimiento/desaceleración.
+	# Como buena práctica, reemplaza las acciones de UI con acciones personalizadas para el juego.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
